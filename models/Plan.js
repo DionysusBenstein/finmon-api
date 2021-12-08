34aa0c71b34877c75debd9342e3bddf14ade907a
DionysusBenstein <dionysusbenstein@gmail.com>
2021-12-08 13:49:22 +0300

    Remove plans role

--- models/Plan.js
+++
@@ -1,8 +0,0 @@
-import pkg from 'mongoose';
-const { Schema, model } = pkg;
-
-const Plan = new Schema({
-    value: { type: String, unique: true, default: "Free" },
-});
-
-export default model('Plan', Plan);
