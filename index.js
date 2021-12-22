46033f49149d82f867ef0278f6510de709cc81b8
DionysusBenstein <dionysusbenstein@gmail.com>
2021-12-22 11:46:12 +0300

    Trigger CI

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import router from './routes/index.js';
 import { swaggerUi, specs } from './docs.js';
 
-const app = expres();
+const app = express();
 
 app.use(express.json());
 app.use('/', router);
