3b3dfd7fd35a8445db4cd98b4a2705a7d0c3f05d
DionysusBenstein <dionysusbenstein@gmail.com>
2021-12-27 21:32:23 +0300

    Add format and millis time separately from each other

--- integrations/privat.js
+++ integrations/privat.js
@@ -3,19 +3,30 @@ import Merchant from 'privatbank-api';
 const merchant = new Merchant({
     id: '202130',
     password: '0f5TTa5eu1W98oV07n1m29zw5WR9xxPm',
-    country: 'UA'
+    country: 'UA',
 });
 
 async function getClientInfo(account) {
-    const balance = await merchant.balance('4149439317562795')
+    const balance = await merchant.balance('4149439317562795');
     return balance;
 }
+
 // 4149439317562795
 async function getTransactions(account, from, to) {
-    console.log(from);
-    console.log(to);
     const statements = await merchant.statement(account, from, to);
-    return statements;
+    const { statement: transactions } = JSON.parse(statements).response.data.info.statements;
+
+    for (const transaction of transactions) {
+        let { trandate, trantime } = transaction;
+
+        transaction.formatTime = `${trandate} ${trantime}`;
+        transaction.timeMillis = new Date(`${trandate}T${trantime}`).getTime();
+
+        transaction.trandate = undefined;
+        transaction.trantime = undefined;
+    }
+
+    return transactions;
 }
 
 export { getClientInfo, getTransactions };
