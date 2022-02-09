113d354b36e11ad34f20b9d7aef3678a5e85417f
DionysusBenstein <dionysusbenstein@gmail.com>
2022-02-09 11:58:31 +0300

    Change method

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -19,9 +19,9 @@ router.get('/', controller.getUsers);
 router.get('/:username', controller.getUserInfo);
 router.get('/:username/budget', controller.getBudgetList);
 router.post('/avatar', upload.single('avatar'), controller.uploadAvatar);
-router.patch('/:username/budget', controller.addBudget);
+router.put('/:username/budget', controller.addBudget);
 router.delete('/:username/budget/:id', controller.removeBudget);
-router.patch('/:username/cryptowallet', controller.addCryptowallet);
+router.put('/:username/cryptowallet', controller.addCryptowallet);
 router.delete('/:username/cryptowallet/:address', controller.removeCryptowallet);
 
 export default router;
